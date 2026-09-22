6e99048d11bbb7555e5d533ac1d908e2bb2175da
Ghosthub <wolfproab777@gmail.com>
2026-09-22 19:53:58 +0500

    Change route handling to use regex for all paths

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '.')));
 
-app.get('*', (req, res) => {
+// '*' o'rniga regex /(.*)/ ishlatamiz
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
